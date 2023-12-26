e4adea6af90ded8ab039a6d1046b868a35673905
“Thiago <tvieira@pdmfc.com>
2023-12-26 15:23:52 +0000

    Set rabbitmq and add console

---
+++ admin/app/Console/Commands/FireEvent.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class FireEvent extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fire';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        return 0;
+    }
+}
--- admin/config/queue.php
+++ admin/config/queue.php
@@ -30,6 +30,21 @@ return [
 
     'connections' => [
 
+        'rabbitmq' => [
+            'driver' => 'rabbitmq',
+            'queue' => env('RABBITMQ_QUEUE', 'default'),
+            'connection' => PhpAmqpLib\Connection\AMQPLazyConnection::class,
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'guest'),
+                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
+                ],
+            ],
+        ],
+
         'sync' => [
             'driver' => 'sync',
         ],
